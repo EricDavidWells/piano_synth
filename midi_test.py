d5ddc40a26ebe372aa71b87877025e50cdd23ee1
Unknown <ewells@ualberta.ca>
2018-01-08 12:28:51 -0700

    note filtering function now accounts for two note_on's in a row

--- midi_test.py
+++ midi_test.py
@@ -12,26 +12,27 @@ print(mid.ticks_per_beat)   # ticks per beat, tick is the smallest time unit
 
 note_msgs = []
 for i, track in enumerate(mid.tracks):
-    # print('Track {}: {}'.format(i, track.name))
     for msg in track:
         if 'note' in msg.type:
             note_msgs.append(msg)
-            # print(msg)
+            print(msg)
 
 notes = []
-
+notemax = 127
+basetime = 0
 # seperates note on and off signals into note volume, length, pitch, and time
-for n, msg in enumerate(note_msgs):
-    if msg.type == 'note_on':
+for n, msg in enumerate(note_msgs): # iterate through all note messages
+    basetime += msg.time
+    if msg.type == 'note_on':   # if the msg is to turn note on
         note_length = 0
-        note_pitch = msg.note
-        note_volume = msg.velocity
         for n_, msg_ in enumerate(note_msgs[n+1:], n+1):
             note_length += msg_.time
-            if msg_.note == msg.note and msg_.type == 'note_off':
+            if msg_.note == msg.note and 'note' in msg_.type:
+                note_pitch = msg.note
+                note_volume = msg.velocity
+                note_time = basetime
+                notes.append([note_pitch, note_volume, note_length, note_time])
                 break
 
-        print(note_length)
-        # notes.append([note_length, note_pitch, note_volume])
-
-
+for note in notes:
+    print(note)
