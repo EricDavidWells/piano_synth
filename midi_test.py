4ab8876c76f795735f62660af749f62f5aaca6fa
Unknown <ewells@ualberta.ca>
2018-01-08 10:24:34 -0700

    made sorting function but it is not very robust

--- midi_test.py
+++ midi_test.py
@@ -10,12 +10,28 @@ print(mid.ticks_per_beat)   # ticks per beat, tick is the smallest time unit
 
 # mido.get_output_names()   # gets available output port names
 
-messages = []
-
+note_msgs = []
 for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
+    # print('Track {}: {}'.format(i, track.name))
     for msg in track:
         if 'note' in msg.type:
-            print(msg)
+            note_msgs.append(msg)
+            # print(msg)
+
+notes = []
+
+# seperates note on and off signals into note volume, length, pitch, and time
+for n, msg in enumerate(note_msgs):
+    if msg.type == 'note_on':
+        note_length = 0
+        note_pitch = msg.note
+        note_volume = msg.velocity
+        for n_, msg_ in enumerate(note_msgs[n+1:], n+1):
+            note_length += msg_.time
+            if msg_.note == msg.note and msg_.type == 'note_off':
+                break
+
+        print(note_length)
+        # notes.append([note_length, note_pitch, note_volume])
 
 
